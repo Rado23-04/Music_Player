113d915a3fca120cfcb36cc12d3f5206666e691b
Rado23-04 <hei.rado.2@gmail.com>
2025-03-12 16:19:34 +0300

    feat: implement function lecture audio with react-native-sound

--- screen/MusicScreen.tsx
+++ screen/MusicScreen.tsx
@@ -1,9 +1,30 @@
-import React from "react";
-import { View, Text, FlatList } from "react-native";
-import useLocalMusic from "../services/getMusic"
+import React, { useState } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import Sound from "react-native-sound";
+import useLocalMusic from "../services/getMusic";
 
 const MusicList = () => {
   const musicFiles = useLocalMusic();
+  const [sound, setSound] = useState<Sound | null>(null);
+
+  const playMusic = (filePath: string) => {
+    if (sound) {
+      sound.stop(() => sound.release()); // Arrêter et libérer l'ancienne musique
+    }
+
+    const newSound = new Sound(filePath, "", (error) => {
+      if (error) {
+        console.error("Erreur lors du chargement du son :", error);
+        return;
+      }
+      newSound.play((success) => {
+        if (!success) console.error("Erreur lors de la lecture du son");
+        newSound.release();
+      });
+    });
+
+    setSound(newSound);
+  };
 
   return (
     <View>
@@ -14,7 +35,9 @@ const MusicList = () => {
         data={musicFiles}
         keyExtractor={(item) => item.path}
         renderItem={({ item }) => (
-          <Text style={{ marginVertical: 5 }}>{item.name}</Text>
+          <TouchableOpacity onPress={() => playMusic(item.path)}>
+            <Text style={{ marginVertical: 5, color: "blue" }}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       />
     </View>
